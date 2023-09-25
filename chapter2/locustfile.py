10e7a7d75199bbab34a01878799a1a0fa8adfaed
ociexplained <ociexplained@gmail.com>
2023-09-25 07:38:41 +0000

    readme

--- chapter2/locustfile.py
+++ chapter2/locustfile.py
@@ -8,5 +8,5 @@ from locust import HttpUser, task
 class Book(HttpUser):
     @task
     def hello(self):
-	      # /vi/book/hello API를 호출
+        # /vi/book/hello API를 호출
         self.client.get("/v1/book/hello")
